129e715306cc2d13b2d466f1d88575990f1bb3de
AlexanderSehr <asehr@hotmail.de>
2023-11-02 22:51:07 +0100

    Update to latest

---
+++ avm/utilities/pipelines/workflows/Get-CalculatedWorkflowControl.ps1
@@ -0,0 +1,20 @@
+function Get-CalculatedWorkflowControl {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $Commit,
+
+        [Parameter(Mandatory = $true)]
+        [string] $ModulePath
+    )
+
+    $diffFiles = git diff 'origin/main' --name-only -- "$ModulePath/*" | Sort-Object -Unique
+
+    if (-not $diffFiles) {
+        Write-Verbose "No files changed in $ModulePath" -Verbose
+        return
+    }
+
+    return $diffFiles
+}
