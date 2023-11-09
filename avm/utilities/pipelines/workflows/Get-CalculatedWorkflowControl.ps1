2bb347ce1fe720b0d9e3ef63aaadcbde9a0fdcf1
AlexanderSehr <asehr@hotmail.de>
2023-11-09 21:09:01 +0100

    Added documentation

--- avm/utilities/pipelines/workflows/Get-CalculatedWorkflowControl.ps1
+++ avm/utilities/pipelines/workflows/Get-CalculatedWorkflowControl.ps1
@@ -1,3 +1,24 @@
+<#
+.SYNOPSIS
+Based on a `git diff`, calculate which stages to run in the pipeline
+
+.DESCRIPTION
+Based on a `git diff`, calculate which stages to run in the pipeline
+- If any files that justify a deployment test have changed, run all tests (e.g., bicep files, ARM template files)
+- Else, if any files that justify a static test have changed, run static tests only (e.g., markdown files, unit test files)
+- Else run no tests
+
+.PARAMETER Commit
+Mandatory. The commit to compare
+
+.PARAMETER ModulePath
+Mandatory. The path to filter changed files down to
+
+.EXAMPLE
+Get-CalculatedWorkflowControl -Commit 'e1f088f7f807db040e79e17d28a656d40dbb2cd8' -ModulePath 'avm/res/key-vault/vault'
+
+Calculate which pipeline stages should be run based on the currently changed files.
+#>
 function Get-CalculatedWorkflowControl {
 
     [CmdletBinding()]
@@ -10,6 +31,7 @@ function Get-CalculatedWorkflowControl {
     )
 
     $calculatedAction = 'runNoTests'
+    # TODO: What to do with the `Commit param` ?
     if ($diffFiles = git diff 'origin/main' --name-only -- $ModulePath | Sort-Object -Unique) {
 
         $bicepTemplateRegex = '(.+\.bicep)'
